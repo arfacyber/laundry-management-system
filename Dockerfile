51c2ae3129e956472becc2b6a52c926d0722bf14
Arfa <rizqitammagiari@gmail.com>
2026-09-09 06:42:02 +0700

    Add Dockerfile and deploy scripts for Render

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM richarvey/nginx-php-fpm:3.1.6
+
+COPY . /var/www/html
+
+ENV WEBROOT /var/www/html/public
+ENV PHP_ERRORS_STDERR 1
+ENV RUN_SCRIPTS 1
+ENV REAL_IP_HEADER 1
+
+WORKDIR /var/www/html
+
+EXPOSE 80
---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+echo "Running composer..."
+composer install --no-dev --optimize-autoloader
+
+echo "Caching config & routes..."
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+echo "Linking storage..."
+php artisan storage:link
+
+echo "Running migrations..."
+php artisan migrate --force
