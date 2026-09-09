5865ebff5e3064b64d12d3107b66a6e8ff006e15
Arfa <rizqitammagiari@gmail.com>
2026-09-09 11:48:04 +0700

    Force Blade compiler to use safe storage path

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Paksa jalur cache tampilan (compiled views) ke direktori storage internal
+        config(['view.compiled' => base_path('storage/framework/views')]);
+
+        // Otomatis buat direktori jika kontainer Docker kehilangan foldernya
+        if (!is_dir(config('view.compiled'))) {
+            @mkdir(config('view.compiled'), 0775, true);
+        }
     }
 }
